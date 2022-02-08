aabeb99a132cb6022791fca2a2f862560fb360e3
Oussama Sallak <93767526+osallak@users.noreply.github.com>
2022-02-08 16:21:36 +0100

    Update ft_atoi.c

--- ft_atoi.c
+++ ft_atoi.c
@@ -23,15 +23,9 @@ int	ft_atoi(const char *str)
 			|| str[i] == '\f' || str[i] == '\n' || str[i] == '\r'))
 		i++;
 	if (str[i] && (str[i] == '-' || str[i] == '+'))
-	{
-		if (str[i] == '-')
+		if (str[i++] == '-')
 			sign *= -1;
-		i++;
-	}
 	while (str[i] && (str[i] >= '0' && str[i] <= '9'))
-	{
-		res = res * 10 + str[i] - '0';
-		i++;
-	}
+		res = res * 10 + str[i++] - '0';
 	return (res * sign);
 }
